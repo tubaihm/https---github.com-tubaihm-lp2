4449f7446e0c86ff08bbdbf0b7fc1198d5c065f5
mayu.hiruta <k.khg42.206@gmail.com>
2024-04-13 20:33:54 +0900

    sp表示　css修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,8 +2,10 @@
 $('.carousel').slick({
     autoplay: false,
     arrows: true,
-    dots: true, // ドットを表示
-    fade: true, // フェードで切り替え
+    dots: true,
+    fade: true,
+    prevArrow: '<img src="/assets/image/voice/arrow-left-circle-fill.svg" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="/assets/image/voice/arrow-right-circle-fill.svg" class="slide-arrow next-arrow">',
 });
 
 // アコーディオンメニュー
@@ -15,13 +17,13 @@ $('.question').on('click', function (e) {
 });
 
 
-// ページトップへ戻る
-var buttonToTop = $('#button-to-top');
-var position = 0;
-var speed = 600;
-buttonToTop.on('click', function () {
-    $("html, body").animate({
-        scrollTop: position
-    }, speed, "swing");
-    return false;
-});
+// // ページトップへ戻る
+// var buttonToTop = $('#button-to-top');
+// var position = 0;
+// var speed = 600;
+// buttonToTop.on('click', function () {
+//     $("html, body").animate({
+//         scrollTop: position
+//     }, speed, "swing");
+//     return false;
+// });
