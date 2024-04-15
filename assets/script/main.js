8596afefc25ceff76e15dac9712acc169ff8f512
mayu.hiruta <k.khg42.206@gmail.com>
2024-04-15 21:19:04 +0900

    pc表示修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,25 @@
-// カルーセル
-$('.carousel').slick({
-    autoplay: false,
-    arrows: true,
-    dots: true,
-    fade: true,
-    prevArrow: '<img src="/assets/image/voice/arrow-left-circle-fill.svg" class="slide-arrow prev-arrow">',
-    nextArrow: '<img src="/assets/image/voice/arrow-right-circle-fill.svg" class="slide-arrow next-arrow">',
+// slickslider
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: false,
+        arrows: true,
+        dots: true,
+        fade: true,
+        prevArrow: '<img src="/assets/image/voice/arrow-left-circle-fill.svg" class="slide-arrow prev-arrow">',
+        nextArrow: '<img src="/assets/image/voice/arrow-right-circle-fill.svg" class="slide-arrow next-arrow">',
+        infinite: true, 
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
 });
 
 // アコーディオンメニュー
@@ -17,13 +31,13 @@ $('.question').on('click', function (e) {
 });
 
 
-// // ページトップへ戻る
-// var buttonToTop = $('#button-to-top');
-// var position = 0;
-// var speed = 600;
-// buttonToTop.on('click', function () {
-//     $("html, body").animate({
-//         scrollTop: position
-//     }, speed, "swing");
-//     return false;
-// });
+$(document).ready(function() {
+    $('#button-to-top').hide();
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#button-to-top').fadeIn();
+        } else {
+            $('#button-to-top').fadeOut();
+        }
+    });
+});
